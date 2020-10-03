904eda1463fb3a9d24daf0203930ddbfeab0f9f3
harleeninfo <varma.179@gmail.com>
2020-10-03 16:38:35 +0530

    starting stage of spring boot

---
+++ jobskills/src/main/java/com/jobskills/JobskillsApplication.java
@@ -0,0 +1,13 @@
+package com.jobskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobskillsApplication.class, args);
+	}
+
+}
---
+++ jobskills/src/test/java/com/jobskills/JobskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
